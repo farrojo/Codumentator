db6e082fb74bf0c8c7dfa0bb393eb6f278121694
frockdevman <rojcof@gmail.com>
2025-02-20 14:55:13 -0500

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 # setup.py
+"""
+Script de instalación del proyecto Codumentator.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="codumentator",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+)
