d563a257b1b5653559ccbdf8d7764c3ef15bcf08
frockdevman <rojcof@gmail.com>
2025-02-20 14:54:59 -0500

    main.py updated

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
 # main.py
+"""
+Punto de entrada principal del sistema de documentación Codumentator.
+"""
+
+from src.generator.doc_generator import generate_documentation
+
+if __name__ == "__main__":
+    print("Codumentator iniciado...")
